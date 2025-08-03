76c106852b5aa34550cc24a74bfe4e64cdf7f26e
mohamad massoidi <mohamadmassoudi90@gmail.com>
2025-08-03 06:34:00 +0330

    navigate user to back   withe custom hook done

--- frontend/start/src/hooks/useMoveBack.js
+++ frontend/start/src/hooks/useMoveBack.js
@@ -3,4 +3,5 @@ import { useRouter } from "next/navigation";
 export default function useMoveBack() {
   const router = useRouter();
   return () => router.back();
+  // navigate user to back   withe custom hook . 
 }
